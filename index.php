1591bf4eec33b60e7cda2fb88d87d6943e239e71
wigamu <wigamu@hotmail.com>
2016-09-11 21:54:22 -0500

    boton

--- index.php
+++ index.php
@@ -35,6 +35,13 @@
                                 echo '<td>'. $row['email'] . '</td>';
                                 echo '<td>'. $row['mobile'] . '</td>';
                                 echo '<td><a class="btn" href="read.php?id='.$row['id'].'">Read</a></td>';
+                                echo '<td width=250>';
+                                echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
+                                echo '</td>';
                                 echo '</tr>';
                        }
                        Database::disconnect();
