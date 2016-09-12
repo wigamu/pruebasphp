cc2362cf12f5881393aa03eec3be43e44fb065e0
wigamu <wigamu@hotmail.com>
2016-09-11 21:16:01 -0500

    incluir

--- create.php
+++ create.php
@@ -4,6 +4,54 @@
     <meta charset="utf-8">
     <link   href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.min.js"></script>
+
+	<?php
+     
+    require 'database.php';
+ 
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $nameError = null;
+        $emailError = null;
+        $mobileError = null;
+         
+        // keep track post values
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
+         
+        // validate input
+        $valid = true;
+        if (empty($name)) {
+            $nameError = 'Please enter Name';
+            $valid = false;
+        }
+         
+        if (empty($email)) {
+            $emailError = 'Please enter Email Address';
+            $valid = false;
+        } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $emailError = 'Please enter a valid Email Address';
+            $valid = false;
+        }
+         
+        if (empty($mobile)) {
+            $mobileError = 'Please enter Mobile Number';
+            $valid = false;
+        }
+         
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO customers (name,email,mobile) values(?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($name,$email,$mobile));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    }
+?>
 </head>
  
 <body>
