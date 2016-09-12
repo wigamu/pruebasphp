55b521db7eb36d569c35bb088b7d77ca637314be
wigamu <wigamu@hotmail.com>
2016-09-11 21:06:40 -0500

    Boton Crear

--- index.php
+++ index.php
@@ -12,30 +12,35 @@
                 <h3>PHP CRUD Grid</h3>
             </div>
             <div class="row">
+                <p>
+                    <a href="create.php" class="btn btn-success">Create</a>
+                </p>
                 <table class="table table-striped table-bordered">
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Email Address</th>
-                      <th>Mobile Number</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  <?php
-                   include 'database.php';
-                   $pdo = Database::connect();
-                   $sql = 'SELECT * FROM customers ORDER BY id DESC';
-                   foreach ($pdo->query($sql) as $row) {
-                            echo '<tr>';
-                            echo '<td>'. $row['name'] . '</td>';
-                            echo '<td>'. $row['email'] . '</td>';
-                            echo '<td>'. $row['mobile'] . '</td>';
-                            echo '</tr>';
-                   }
-                   Database::disconnect();
-                  ?>
-                  </tbody>
-            </table>
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Email Address</th>
+                          <th>Mobile Number</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      <?php
+                       include 'database.php';
+                       $pdo = Database::connect();
+                       $sql = 'SELECT * FROM customers ORDER BY id DESC';
+                       foreach ($pdo->query($sql) as $row) {
+                                echo '<tr>';
+                                echo '<td>'. $row['name'] . '</td>';
+                                echo '<td>'. $row['email'] . '</td>';
+                                echo '<td>'. $row['mobile'] . '</td>';
+                                echo '<td><a class="btn" href="read.php?id='.$row['id'].'">Read</a></td>';
+                                echo '</tr>';
+                       }
+                       Database::disconnect();
+                      ?>
+                      </tbody>
+                </table>
         </div>
     </div> <!-- /container -->
   </body>
