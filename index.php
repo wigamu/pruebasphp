d51e4115ff97fcee1c29f4f71dbcb60efd1755c9
wigamu <wigamu@hotmail.com>
2016-09-11 21:57:36 -0500

    Boton

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
                 <p>
                     <a href="create.php" class="btn btn-success">Create</a>
                 </p>
+                 
                 <table class="table table-striped table-bordered">
                       <thead>
                         <tr>
@@ -34,14 +35,13 @@
                                 echo '<td>'. $row['name'] . '</td>';
                                 echo '<td>'. $row['email'] . '</td>';
                                 echo '<td>'. $row['mobile'] . '</td>';
-                                echo '<td><a class="btn" href="read.php?id='.$row['id'].'">Read</a></td>';
                                 echo '<td width=250>';
                                 echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
-                                echo '</td>';
+                                echo ' ';
+                                echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
+                                echo '</td>';
                                 echo '</tr>';
                        }
                        Database::disconnect();
